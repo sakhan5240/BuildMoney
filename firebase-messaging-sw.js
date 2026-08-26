1ef9f404c38d438f783e7b241162332d7bad26b9
Sahabbar Ali <educationallvowel@gmail.com>
2026-08-26 06:17:12 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,7 @@
 // ==========================================
 // 1. OFFLINE CACHING ENGINE (No Internet Dinosaur Fix)
 // ==========================================
-const CACHE_NAME = 'buildmoney-offline-cache-v9';
+const CACHE_NAME = 'buildmoney-offline-cache-v10';
 const urlsToCache = [
     './',
     './index.html',
